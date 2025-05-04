ff80ec74d300a78f0206e5c24cf7c34813a297bf
Yu Chen <ione0213@gmail.com>
2025-05-04 17:28:15 +0800

    fix: remove base for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/yu-chen-wedding/' : '/',
   plugins: [react()],
 })
